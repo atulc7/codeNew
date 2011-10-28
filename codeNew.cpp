9a6c5de45ce24b6768ce5035e407b5af277d9bfb
AtulC <atul.choudhary@gmail.com>
2011-10-28 21:42:40 +0530

    my commit

--- codeNew.cpp
+++ codeNew.cpp
@@ -8,54 +8,32 @@
 
 using namespace std;
 
-
 string fileOp::createFile()
 {
-char name[20];
-string LINE;
-cout << " Enter the name of the header file: " << endl;
-cin >> name;
-ofstream myHeader;
-ifstream myConfig;
-myHeader.open(name, ios::out);
-if (myHeader.is_open())
-{
-cout << " File opened successfully. " << endl;
-myConfig.open("conf.txt");
-//while (myHeader.good())
+	char name[20];
+	string LINE;
+	cout << " Enter the name of the header file: " << endl;
+	cin >> name;
+	ofstream myHeader;
+	ifstream myConfig;
+	myHeader.open(name, ios::out);
+	if (myHeader.is_open())
 	{
-	while (myConfig.good())
+		cout << " File opened successfully. " << endl;
+		myConfig.open("conf.txt");
 		{
-		getline (myConfig, LINE, '\n');
-		cout << LINE << endl;
-		myHeader << "#" << LINE << endl;
+			while (myConfig.good())
+			{
+				getline (myConfig, LINE, '\n');
+				cout << LINE << endl;
+				myHeader << "#" << LINE << endl;
+			}
 		}
-//	myHeader << LINE << endl;
+		myHeader.close();
 	}
-myHeader.close();
-}
-else
-cout <<" Unable to open the file. " << endl;
-return name;
+	else
+	cout <<" Unable to open the file. " << endl;
+	return name;
 }
 
-/*------------------------------------------------------------------------------
-void fileOp::readConfFile(string head)
-{
-string LINE;
-string HEAD;
-
-HEAD = head;
-ifstream myConfig;
-ofstream myHeader;
-myConfig.open("conf.txt");
-myHeader.open(HEAD, ios::app);
-while (myConfig.good())
-	{
-	getline (myConfig, LINE, '\n');
-	myHeader << "\n" << LINE;
-	}
-}	
-
-
-*/
+/*--------------------------------------------------------------------*/
